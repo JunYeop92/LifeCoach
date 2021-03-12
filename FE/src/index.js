b234465a1eccd9279d57260ff2b3362b4a2365d8
JunYeop92 <41728258+JunYeop92@users.noreply.github.com>
2021-03-12 17:25:16 +0900

    웹 디자인 개발용

--- FE/src/index.js
+++ FE/src/index.js
@@ -1,60 +1,64 @@
 import Timer from './component/timer/Timer.js';
 import Category from './component/category/Category.js';
-import css from './style.css';
+import './Normalize.css';
+import './style.css';
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/brands'
 // new Category(document.querySelector('#app'));
 // new Timer(document.querySelector('#app'));
 
-(function () {
-    const root = document.querySelector('#app');
+// (function () {
+//     const root = document.querySelector('#app');
 
-    const timer = new Timer();
-    const category = new Category();
+//     const timer = new Timer();
+//     const category = new Category();
 
-    const routes = {
-        // path: url
-        '/': timer,
-        '/category': category,
-    };
+//     const routes = {
+//         // path: url
+//         '/': timer,
+//         '/category': category,
+//     };
 
-    const render = async (path) => {
-        root.innerHTML = '';
-        const renderComponent = routes[path];
-        renderComponent.render(root);
+//     const render = async (path) => {
+//         root.innerHTML = '';
+//         const renderComponent = routes[path];
+//         renderComponent.render(root);
 
-        // if (!renderComponent) {
-        //     root.innerHTML = `${path} Not Found`;
-        //     return;
-        // }
-    };
+//         // if (!renderComponent) {
+//         //     root.innerHTML = `${path} Not Found`;
+//         //     return;
+//         // }
+//     };
 
-    // popstate 이벤트는 history entry가 변경되면 발생한다.
-    // PJAX 방식은 hash를 사용하지 않으므로 hashchange 이벤트를 사용할 수 없다.
-    // popstate 이벤트는 pushState에 의해 발생하지 않는다.
-    // 이전페이지 / 다음페이지 button 또는 history.back() / history.go(n)에 의해 발생한다.
-    window.addEventListener('popstate', (e) => {
-        // e.state는 pushState 메서드의 첫번째 인수
-        console.log('[popstate]', e.state);
-        // 이전페이지 / 다음페이지 button이 클릭되면 render를 호출
-        render(e.state.path);
-    });
+//     // popstate 이벤트는 history entry가 변경되면 발생한다.
+//     // PJAX 방식은 hash를 사용하지 않으므로 hashchange 이벤트를 사용할 수 없다.
+//     // popstate 이벤트는 pushState에 의해 발생하지 않는다.
+//     // 이전페이지 / 다음페이지 button 또는 history.back() / history.go(n)에 의해 발생한다.
+//     window.addEventListener('popstate', (e) => {
+//         // e.state는 pushState 메서드의 첫번째 인수
+//         console.log('[popstate]', e.state);
+//         // 이전페이지 / 다음페이지 button이 클릭되면 render를 호출
+//         render(e.state.path);
+//     });
 
-    // 네비게이션을 클릭하면 주소창의 url이 변경되므로 HTTP 요청이 서버로 전송된다.
-    // preventDefault를 사용하여 이를 방지하고 history 관리를 위한 처리를 실시한다.
-    navigation.addEventListener('click', (e) => {
-        if (!e.target.matches('#navigation > li > a')) return;
-        e.preventDefault();
-        // 이동 페이지
-        const path = e.target.getAttribute('href');
+//     // 네비게이션을 클릭하면 주소창의 url이 변경되므로 HTTP 요청이 서버로 전송된다.
+//     // preventDefault를 사용하여 이를 방지하고 history 관리를 위한 처리를 실시한다.
+//     navigation.addEventListener('click', (e) => {
+//         if (!e.target.matches('#navigation > li > a')) return;
+//         e.preventDefault();
+//         // 이동 페이지
+//         const path = e.target.getAttribute('href');
 
-        // 주소창의 url은 변경되지만 HTTP 요청이 서버로 전송되지는 않는다.
-        history.pushState({ path }, null, path);
-        // path에 의한 AJAX 요청
-        render(path);
-    });
+//         // 주소창의 url은 변경되지만 HTTP 요청이 서버로 전송되지는 않는다.
+//         history.pushState({ path }, null, path);
+//         // path에 의한 AJAX 요청
+//         render(path);
+//     });
 
-    // 웹페이지가 처음 로딩되었을 때
-    render('/');
+//     // 웹페이지가 처음 로딩되었을 때
+//     render('/');
 
-    // 새로고침이 클릭되었을 때, 현 페이지(예를 들어 loclahost:5004/service)가 서버에 요청된다.
-    // 이에 응답하는 기능이 서버 측에 추가되어야 한다.
-})();
+//     // 새로고침이 클릭되었을 때, 현 페이지(예를 들어 loclahost:5004/service)가 서버에 요청된다.
+//     // 이에 응답하는 기능이 서버 측에 추가되어야 한다.
+// })();
