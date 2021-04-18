68325b3678a8fed638d3da36a9362a64e2888867
JunYeop92 <41728258+JunYeop92@users.noreply.github.com>
2021-04-18 13:57:53 +0900

    [수정] 드롭다운 메뉴 외 클릭 시 이벤트
    무엇을?
    - 아이디가 있을 경우만 이벤트 실행

--- FE/src/component/App.js
+++ FE/src/component/App.js
@@ -1,7 +1,7 @@
 import Timer from './timer/Timer.js';
 import Category from './category/Category.js';
 import { getTodayTime, getWeeklyTime, getRecord } from '../api/time.js';
-import { getYmd, getWeek } from "../util.js";
+import { getYmd, getWeek } from '../util.js';
 
 export default function App() {
     this.state;
@@ -10,16 +10,19 @@ export default function App() {
     this.initialize = async () => {
         const $root = document.querySelector('#app');
         $root.addEventListener('click', (e) => {
-            const targetId = (e.target.tagName == 'path') ? e.target.parentNode.id : e.target.id;
+            const targetId =
+                (e.target.tagName == 'path') ? e.target.parentNode.id : e.target.id;
 
-            const $categoryBtn = e.currentTarget.querySelector('#category .dropbtn');
-            if(!($categoryBtn.querySelector(`#${targetId}`)) ){
-                $categoryBtn.classList.remove('click');
-            }
+            if (targetId) {
+                const $categoryBtn = e.currentTarget.querySelector('#category .dropbtn');
+                if (!$categoryBtn.querySelector(`#${targetId}`)) {
+                    $categoryBtn.classList.remove('click');
+                }
 
-            const $recordBtn = e.currentTarget.querySelector('#record .dropbtn');
-            if(!($recordBtn.querySelector(`#${targetId}`)) ){
-                $recordBtn.classList.remove('click');
+                const $recordBtn = e.currentTarget.querySelector('#record .dropbtn');
+                if (!$recordBtn.querySelector(`#${targetId}`)) {
+                    $recordBtn.classList.remove('click');
+                }
             }
         });
         const $header = document.createElement('div');
@@ -36,7 +39,7 @@ export default function App() {
 
         const category = new Category({
             $target: { $header },
-            updateTimer
+            updateTimer,
         });
         const timer = new Timer({
             $target: { $header, $content },
@@ -47,15 +50,15 @@ export default function App() {
         };
     };
 
-    const updateTimer = async ({categoryList, _id, name}) => {
-        const {timer} = this.component;
+    const updateTimer = async ({ categoryList, _id, name }) => {
+        const { timer } = this.component;
         console.log('오류');
         console.log(categoryList);
         const categoryId = _id || categoryList[0]._id;
         const categoryName = name || categoryList[0].content;
-        
+
         const ymd = getYmd(new Date());
-        const {startWeekDate, endWeekDate} = getWeek();
+        const { startWeekDate, endWeekDate } = getWeek();
         const startYmd = getYmd(startWeekDate);
         const endYmd = getYmd(endWeekDate);
 
@@ -68,16 +71,16 @@ export default function App() {
             startYmd,
             endYmd,
         });
-        const resultRecord = await getRecord({categoryId});
+        const resultRecord = await getRecord({ categoryId });
 
         timer.setState({
             ...timer.state,
             selectedCategory: { _id: categoryId, name: categoryName },
             recordList: resultRecord.data,
             todayTime: resultToday.data,
-            weeklyTime : resultWeekly.data
+            weeklyTime: resultWeekly.data,
         });
-    }
+    };
 
     this.setState = () => {};
     this.render = () => {};
@@ -85,9 +88,6 @@ export default function App() {
     this.initialize();
 }
 
-
-
- 
 // test code
 // var Dates = new Date().getWeek();
 // alert(Dates[0].toLocaleDateString() + ' to '+ Dates[1].toLocaleDateString());
